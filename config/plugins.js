a8d71a7f9e0f5586a07a399520ad8211bb9c5e5e
muhammadabrar <42229174+muhammadabrar@users.noreply.github.com>
2024-06-12 09:46:48 +0500

    S3 and postgres added

--- config/plugins.js
+++ config/plugins.js
@@ -1,1 +1,33 @@
-module.exports = () => ({});
+module.exports = ({ env }) => ({
+  upload: {
+    config: {
+      provider: 'aws-s3',
+      providerOptions: {
+        accessKeyId: env('AWS_ACCESS_KEY_ID'),
+        secretAccessKey: env('AWS_ACCESS_SECRET'),
+        region: env('AWS_REGION'),
+        params: {
+          ACL: env('AWS_ACL', 'public-read'),
+          signedUrlExpires: env('AWS_SIGNED_URL_EXPIRES', 15 * 60),
+          Bucket: env('AWS_BUCKET'),
+        },
+      },
+      actionOptions: {
+        upload: {},
+        uploadStream: {},
+        delete: {},
+      },
+    },
+  },
+
+localazy: {
+    config: {
+      /**
+       * both options may help guard against DoS attacks
+       * if `populateMaxDepth` < 5; the Localazy Strapi Plugin may not work as expected
+       */
+      populateDefaultDepth: 5, // default is 5
+      populateMaxDepth: 10, // default is 10
+    },
+  },
+  });
